f36b68ceb5985e567cb47786ee6c5b1aff9abe71
Harun Doğdu <harundogdu06@gmail.com>
2022-01-11 16:31:12 +0300

    project component created

---
+++ components/Home/Project.js
@@ -0,0 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+import React from "react";
+import ProjectImage from "../../public/projects.png";
+const Project = ({ project }) => {
+  console.log(ProjectImage);
+  return (
+    <a
+      href={project.url}
+      target="_blank"
+      className="flex flex-col border border-primary border-opacity-10 py-3 w-full h-[300px] text-center px-4 rounded-lg  hover:bg-primary hover:text-white group transition-all"
+      rel="noreferrer"
+    >
+      <div className="w-full h-1/2 mb-4 flex items-center py-2">
+        <img
+          src={ProjectImage.src}
+          alt={"Project" + project.title}
+          className="w-1/2 object-cover mx-auto"
+        />
+      </div>
+      <div className="py-4">
+        <h2 className="text-xl text-primary group-hover:text-white font-semibold">
+          {project.title}
+        </h2>
+        <p className="my-2 text-sm px-4">{project.description}</p>
+      </div>
+    </a>
+  );
+};
+
+export default Project;
