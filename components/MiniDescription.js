ed5c7d344f5a00d6add8217b510f5a068d5e1e42
Harun Doğdu <harundogdu06@gmail.com>
2022-01-11 21:02:36 +0300

    miniDescription component added

---
+++ components/MiniDescription.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MiniDescription = () => {
+  return (
+    <p className="w-96 my-2 font-thin">
+      Apps developer. Loves good and intuitive UX. Makes fun things to IRL. Also,
+      trader and gamer.
+    </p>
+  );
+};
+
+export default MiniDescription;
