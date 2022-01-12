2079916df0873aeeee3217b07c72bcae74d8e55a
Harun Doğdu <harundogdu06@gmail.com>
2022-01-12 17:33:25 +0300

    img added

--- components/Home/HomeHeader.js
+++ components/Home/HomeHeader.js
@@ -4,24 +4,37 @@ import React, { useState } from "react";
 
 const HomeHeader = () => {
   const { techs } = useMainContext();
-  return (
+  //
+  https: return (
     <>
       <h1 className="text-6xl font-extrabold">
         Full-stack web and Mobile apps developer.
       </h1>
-      <p className="pr-48 py-4">
-        I'm a full-stack web & mobile apps developer with over 5 years of
-        working experience. Currently working on a bunch of productivity-rated
-        apps that are used by thousands of professionals worldwide. A good;
-        speaker, reader, listener and friend. He likes to learn new technologies
-        and teach what he learns. You can contact at any time.
-      </p>
-      <div className="flex gap-x-4">
-        {techs.map((item) => (
-          <span key={item.id} className="text-primary text-lg font-bold">
-            #{item.title}
-          </span>
-        ))}
+      <div className="flex">
+        <div className="flex flex-col">
+          <p className="pr-48 py-4">
+            I'm a full-stack web & mobile apps developer with over 5 years of
+            working experience. Currently working on a bunch of
+            productivity-rated apps that are used by thousands of professionals
+            worldwide. A good; speaker, reader, listener and friend. He likes to
+            learn new technologies and teach what he learns. You can contact at
+            any time.
+          </p>
+          <div className="flex gap-x-4">
+            {techs.map((item) => (
+              <span key={item.id} className="text-primary text-lg font-bold">
+                #{item.title}
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className="">
+          <img
+            src="https://i.ibb.co/3M5xWYR/d447ee50-4c07-49d4-8f37-35dc530fbbf4.jpg"
+            className="w-full"
+            alt="Mami"
+          />
+        </div>
       </div>
     </>
   );
