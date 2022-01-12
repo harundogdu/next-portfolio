5934052603951d1afabd1cb45626231057e9b615
Harun Doğdu <harundogdu06@gmail.com>
2022-01-12 16:18:47 +0300

    api changes v1

--- enviroments/index.js
+++ enviroments/index.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://harundogdu.com"
+    ? "https://jsonplaceholder.typicode.com"
     : "http://localhost:3000";
 
 export { BASE_URL };
--- pages/work/[slug].js
+++ pages/work/[slug].js
@@ -22,7 +22,7 @@ const ProjectDetails = ({ data }) => {
 };
 
 export async function getServerSideProps({ params }) {
-  const response = await fetch(`${BASE_URL}/api/project/${params.slug}`);
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/1`);
   const data = await response.json();
 
   return {
